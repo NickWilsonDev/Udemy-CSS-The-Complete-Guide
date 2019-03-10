97bcda78e52893ada337abcc48447ca98d9f4a63
Nick Wilson <logikfx@gmail.com>
2019-03-10 17:01:59 -0400

    Added basic animations and supports tags to project.

--- shared.js
+++ shared.js
@@ -4,13 +4,15 @@ let modalNoButton = document.querySelector('.modal__action--negative');
 let toggleButton = document.querySelector('.toggle-button');
 let selectPlanButtons = document.querySelectorAll('.plan button');
 let mobileNav = document.querySelector('.mobile-nav');
-
+let ctaButton = document.querySelector(".main-nav__item--cta");
 
 
 for (let i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', function() {
-        modal.style.display = 'block';
-        backdrop.style.display = 'block';
+        //modal.style.display = 'block';
+        //backdrop.style.display = 'block';
+        modal.classList.add("open");
+        backdrop.classList.add("open");
     });
 }
 
@@ -25,11 +27,25 @@ if (modalNoButton) {
 
 function closeModal() {
     if (modal) {
-        modal.style.display = 'none';
+       // modal.style.display = 'none';
+        modal.classList.remove("open");
     }
-     backdrop.style.display = 'none';
+     //backdrop.style.display = 'none';
+    backdrop.classList.remove("open");
 }
 toggleButton.addEventListener('click', function() {
     mobileNav.style.display = 'block';
     backdrop.style.display = 'block';
+});
+
+ctaButton.addEventListener('animationstart', function(event) {
+    console.log('animation started', event);
+});
+
+ctaButton.addEventListener('animationend', function(event) {
+    console.log('animation ended', event);
+});
+
+ctaButton.addEventListener('animationiteration', function(event) {
+    console.log('animation iteration', event);
 });
